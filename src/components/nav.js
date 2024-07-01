5a29b4b6abb595b7d42d1bd784ed5e5792d33444
Daniel Ramsgard <Daniel.Ramsgard@unc.edu>
2024-07-01 14:58:20 -0400

    dropdowns

--- src/components/nav.js
+++ src/components/nav.js
@@ -44,6 +44,13 @@ const Nav = () => {
                                         <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"/>
                                     </svg>
                                 </div>
+                                <div className={ chevron ? "dropdown" : "dropdown-no"}>
+                                { chevron ? 
+                                <>
+                                <div className="drop-text">Cognitive Behavioral Therapy</div> 
+                                <div className="drop-text">Interpersonal Therapy</div>
+                                </> : <></>}
+                                </div>
                             </div>
                             <div className="nav-text" onMouseEnter={() => {
                                     setChevronTwo(true);
@@ -57,6 +64,14 @@ const Nav = () => {
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                                         <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"/>
                                     </svg>
+                                </div>
+                                <div className={ chevronTwo ? "dropdown-special" : "dropdown-no-special"}>
+                                { chevronTwo ? 
+                                <>
+                                <div className="drop-text">Patient Intake Forms</div> 
+                                <div className="drop-text">Schedule Consultation</div>
+                                <div className="drop-text">Contact Dr. Aspenleiter</div>
+                                </> : <></>}
                                 </div>
                             </div>
                             <div className="nav-text">
