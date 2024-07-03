9bc1310ffb32321deb33d0a509447cb30727b53e
Daniel Ramsgard <Daniel.Ramsgard@unc.edu>
2024-07-03 10:26:27 -0400

    work

---
+++ src/components/bottomHeader.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const BottomHeader = () => {
+    return (
+        <>
+            <div className="bottom-header">
+                
+            </div>
+        </>
+    );
+}
+
+export default BottomHeader;
--- src/components/home.js
+++ src/components/home.js
@@ -2,11 +2,14 @@ import React from "react";
 import useWindowWidth from "./useWindowWidth";
 import Card from "./card";
 import UserMap from "./map";
+import BottomHeader from "./bottomHeader";
 
 const Home = () => {
     const width = useWindowWidth(945);
     const widthTwo = useWindowWidth(700);
     const widthThree = useWindowWidth(485);
+    const widthFour = useWindowWidth(970);
+    const widthSix = useWindowWidth(590);
     const path = "/static/media/";
     const specialties = [
         {
@@ -112,7 +115,7 @@ const Home = () => {
             <div className="specialties">
                 <div className="specialties-container">
                     <div className="landing-title title">
-                        Our Locations
+                        Her Locations
                     </div>
                     <div className="spacer-3"></div>
                     <div className="expertise">
@@ -133,16 +136,34 @@ const Home = () => {
                         <div className="about-img-container">
                             <img alt="Dr. Julie Ann Aspenlieter, PysD" src="/static/media/dr.jpeg" className="about-img"></img>
                         </div>
+                        {widthFour ? widthSix ? <div className="grey-2"></div> : <div className="grey-bar-new"></div> : <div className="grey-bar"></div>}
                         <div className="header-text-2">
                             Dr. Aspenleiter is a distinguished clinical psychologist known for her empathetic approach and expertise in human behavior. She integrates advanced therapeutic techniques with compassionate care, empowering patients to navigate challenges with resilience. Her contributions to the field reflect a commitment to holistic healing and personalized treatment.
+                        </div>
+                        
+                        {widthFour ? widthSix ? <div className="grey-2"></div> : <div className="grey-bar-new"></div> : <div className="grey-bar"></div>}
+                        <div className="container">
+                            <div>
+                                <h2>Contact</h2>
+                                <p> <div className="new-color"> Fill Out Intake Forms </div>
+                                    New Patients </p>
+                                <p> <div className="new-color"> GIVE HER A CALL </div>
+                                (315) 857-7864</p>
+                            </div>
+                            <div>
+                                <h2>Locations</h2>
+                                <p><div className="new-color"> 3460 County Line Rd </div>
+                                Skaneateles, NY 13152
+                                </p>
+                                <p><div className="new-color"> 314 E. Seneca Street </div>
+                                Manlius, New York 13104</p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
             <div className="spacer-2"></div>
-            <div className="bottom-header">
-
-            </div>
+            <BottomHeader />
         </>
     );
 }
